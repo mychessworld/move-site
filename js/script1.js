920d4e6b05e0d17ba281003204d240c3c570fe83
mychessworld <mychessworld@yahoo.com>
2022-09-04 17:35:57 +1000

    first commit

---
+++ js/script1.js
@@ -0,0 +1,13 @@
+(function () {'use strict';}());
+
+// const header = document.getElementsByClassName('header');
+// const header__search = document.getElementsByClassName('header__search');
+// const header2 = document.querySelector('');
+// const header3 = document.querySelector('');
+// const header4 = document.querySelector('[placeholder="Поиск"]');
+const header5 = document.querySelector('.promo__title');
+
+
+console.log(header5);
+
+
